481f582632215617565a3f85c9ff4137a9de927e
mariambuzaladze <mariambuzaladze10@example.come>
2024-01-18 00:44:26 +0400

    kind of fix photo upload

--- pages/addNewBlog/addNewBlog.js
+++ pages/addNewBlog/addNewBlog.js
@@ -1,21 +1,31 @@
-const dropArea = document.getElementById("drop-area");
 const inputFile = document.getElementById("input-file");
 const imageView = document.getElementById("image-view");
+const uploadedImage = document.getElementById("uploaded-image");
 
 inputFile.addEventListener("change", uploadImage);
 
 function uploadImage() {
-  let imgLink = URL.createObjectURL(inputFile.files[0]);
-  imageView.style.backgroundImage = `url(${imgLink})`;
-  imageView.textContent = "";
+  const file = inputFile.files[0];
+  if (file) {
+    let imgLink = URL.createObjectURL(file);
+    uploadedImage.src = imgLink;
+    imageView.style.display = "flex"; // Show the image view
+  }
 }
+
+function deleteImage() {
+  imageView.style.display = "none"; // Hide the image view
+  inputFile.value = ""; // Clear the file input
+}
+
+const dropArea = document.getElementById("drop-area");
 
 dropArea.addEventListener("dragover", function (e) {
   e.preventDefault();
 });
 
 dropArea.addEventListener("drop", function (e) {
-  e.preventDefault(); // Prevent the default behavior
+  e.preventDefault();
   inputFile.files = e.dataTransfer.files;
   uploadImage();
 });
