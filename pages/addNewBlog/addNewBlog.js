08a956f9d8d228bfdfb853c11226c58203d18beb
mariambuzaladze <mariambuzaladze10@example.come>
2024-01-21 21:01:52 +0400

    add posting blog

--- pages/addNewBlog/addNewBlog.js
+++ pages/addNewBlog/addNewBlog.js
@@ -1,13 +1,18 @@
-let image = localStorage.getItem("img");
 let author = localStorage.getItem("author");
 let title = localStorage.getItem("title");
 let description = localStorage.getItem("description");
 let publishDate = localStorage.getItem("publish-date");
 let imail = localStorage.getItem("imail");
+let finalCategories = localStorage.getItem("categories");
 
 function restart() {
-  if (image) {
-    imageView.defaultValue = image;
+  let base64String = localStorage.getItem("img");
+  if (base64String) {
+    let img = new Image();
+    img.src = base64String;
+    img.onload = function () {
+      console.log(img);
+    };
   } else {
     imageView.defaultValue = "";
   }
@@ -64,7 +69,7 @@ function hideImage() {
   imageView.style.width = "600px";
   imageView.style.height = "56px";
   imageView.style.background = "#F2F2FA;";
-  imageView.textContent = `${getImage()}`;
+  imageView.textContent = inputFile.files[0].name;
 }
 
 const dropArea = document.getElementById("drop-area");
@@ -221,11 +226,10 @@ publishDateInput.addEventListener("input", function () {
 let categoryInput = document.querySelector(".arrow-down");
 let div = document.querySelector(".categories");
 
-let dropDown = true;
-
+let dropDown = false;
+categories();
 categoryInput.addEventListener("click", function () {
-  if (dropDown) {
-    categories();
+  if (!dropDown) {
     div.style.display = "flex";
     div.style.flexDirection = `column`;
     div.style.gap = "4px";
@@ -233,24 +237,22 @@ categoryInput.addEventListener("click", function () {
     div.style.borderRadius = "10px";
 
     categoryInput.style.transform = "rotate(180deg)";
-    dropDown = false;
-  }
-  if ((dropDown = false)) {
-    div.style.display = "none";
-    categoryInput.style.transform = "rotate(180deg)";
     dropDown = true;
+  } else {
+    div.style.display = "none";
+    categoryInput.style.transform = "rotate(0deg)";
+    dropDown = false;
   }
 });
 
 let response;
 let data;
-
+let theInput = document.querySelector(".category_");
 let categoryArr = [];
 
 async function categories() {
   response = await fetch("https://george.pythonanywhere.com/api/categories/");
   data = await response.json();
-  console.log(data);
 
   data.forEach((element) => {
     let newButton = document.createElement("button");
@@ -277,16 +279,19 @@ async function categories() {
 
       let paragraph = document.querySelector(".category");
       paragraph.style.display = "none";
-      // categoryInput.style.display = "none";
-      let theInput = document.querySelector(".category_");
       theInput.style.gap = "4px";
 
       theInput.append(xButton);
       categoryArr.push(element);
+      localStorage.setItem("categories", JSON.stringify(categoryArr));
       console.log(categoryArr);
 
       xButton.addEventListener("click", (e) => {
         e.target.remove();
+        let index = categoryArr.findIndex((cat) => cat.title === element.title);
+        categoryArr.splice(index, 1);
+        localStorage.setItem("categories", JSON.stringify(categoryArr));
+        console.log(categoryArr);
       });
     });
 
@@ -295,3 +300,54 @@ async function categories() {
 }
 
 restart();
+
+let uploadButton = document.getElementById("upload");
+let list = document.getElementsByTagName("li");
+
+uploadButton.addEventListener("click", async () => {
+  let isValidationPassed = true;
+
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].style.color !== "green") {
+      isValidationPassed = false;
+      break;
+    }
+  }
+
+  if (isValidationPassed) {
+    const token = "45c70088173aafe84bbf8857f1975ca181bef914";
+    const url = "https://george.pythonanywhere.com/api/blogs/create/";
+
+    const data = {
+      title: `${title}`,
+      author: `${author}`,
+      publish_date: `${publishDate}`,
+      description: `${description}`,
+      imail: `${imail}`,
+      image: localStorage.getItem("img"),
+      categories: JSON.parse(localStorage.getItem("categories")) || [],
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token}`,
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        console.error("An error has occurred:", error);
+        return;
+      }
+
+      const result = await response.json();
+      console.log("Blog created successfully:", result);
+    } catch (error) {
+      console.error("An error occurred during the request:", error);
+    }
+  }
+});
